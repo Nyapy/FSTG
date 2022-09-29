544b48dbbe041e334efabe11b7e82bdce565d8d9
Nyapy <nyapy@naver.com>
2022-09-29 21:42:20 +0900

    Heap Sort

--- PS/Project1/Sort2750.c
+++ PS/Project1/Sort2750.c
@@ -1,19 +1,19 @@
-#if 0
+#if 01
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-int size = 10;
-int arr[10] = { 20, 1, 21, 6, 6, 5, 15, 7, 2, 32 };
+int size;
+int arr[1000];
 
 void heapify(int* a, int size) {
-	for (int i = size/2; i >=0; i--) {
+	for (int i = size / 2; i >= 0; i--) {
 
 		int par = i;
-		int ch = (i*2)+1;
+		int ch = (i * 2) + 1;
 
-		while (ch <= size-1) {
-			if ((ch<size-1) &&(a[ch] < a[ch + 1])) {
+		while (ch <= size - 1) {
+			if ((ch < size - 1) && (a[ch] < a[ch + 1])) {
 				ch++;
 			}
 			if (a[par] < a[ch]) {
@@ -22,25 +22,25 @@ void heapify(int* a, int size) {
 				a[ch] = temp;
 			}
 			par = ch;
-			ch = (par*2)+1;
+			ch = (par * 2) + 1;
 		}
 
 	}
 }
 
-void heapSort(int *a, int size) {
+void heapSort(int* a, int size) {
 	heapify(a, size);
 	int tem;
 	int ch, par;
-	for (int i = size-1; i >0; i--) {
+	for (int i = size - 1; i > 0; i--) {
 		int tem = a[i];
 		a[i] = a[0];
 		a[0] = tem;
 		par = 0;
 		ch = (par * 2) + 1;
 
-		while (ch <= i-1) {
-			if ((ch <i-1) && (a[ch] < a[ch + 1])) {
+		while (ch <= i - 1) {
+			if ((ch < i - 1) && (a[ch] < a[ch + 1])) {
 				ch++;
 			}
 			if (tem < a[ch]) {
@@ -56,11 +56,18 @@ void heapSort(int *a, int size) {
 
 
 int main() {
+	
+	scanf(" %d", &size);
+	for (int i = 0; i < size; i++) {
+		scanf(" %d", &arr[i]);
+	}
+
+	
 	heapSort(arr, size);
 
 
 	for (int i = 0; i < size; i++) {
-		printf("%d ", arr[i]);
+		printf("%d\n", arr[i]);
 	}
 }
 #endif
