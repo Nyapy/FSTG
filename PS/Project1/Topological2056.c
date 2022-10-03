2475d665d0a187beff1a95b745999febe1d1ab94
Nyapy <nyapy@naver.com>
2022-10-04 00:15:10 +0900

    2056 맞왜틀

--- PS/Project1/Topological2056.c
+++ PS/Project1/Topological2056.c
@@ -44,15 +44,29 @@ void addQ(qnode* o, queue* q) {
 	}
 	q->tail = o;
 	(q->size)++;
-
 }
 
+qnode *popQ(queue* q) {
+	if (q->size == 0) {
+		printf("Queue is empty");
+		return NULL;
+	}
+	qnode* ret;
+
+	ret = q->head;
+
+	q->head = q->head->back;
+	q->size--;
+	return ret;
+}
 
 int main() {
 
 	scanf("%d", & N);
 
 	op oper[10001];
+	queue q;
+	q.size = 0;
 
 	for (int i = 1; i <= N; i++) {
 		scanf(" %d %d", &oper[i].time, &oper[i].prio);
@@ -68,10 +82,48 @@ int main() {
 			oper[tem].vec.last = a;
 			oper[tem].vec.size ++;
 		}
+
+		if (oper[i].prio == 0) {
+			qnode *qn= (qnode*)malloc(sizeof(qnode));
+			qn->oper = &oper[i];
+			qn->back = NULL;
+			oper[i].end = 0;
+			addQ(qn, &q);
+		}
 	}
 
+	int ans=0;
+	while (q.size > 0) {
+		qnode *qn = popQ(&q);
+		
+		int cy = qn->oper->vec.size;
+		qn->oper->end += qn->oper->time;
 
+		if (ans < (qn->oper->end)) {
+			ans = qn->oper->end;
+		}
 
+		for (int i = 0; i < cy; i++) {
+			node* a = qn->oper->vec.last;
+
+			(oper[a->op].prio)--;
+			if (oper[a->op].end < qn->oper->end) {
+				oper[a->op].end = qn->oper->end;
+			}
+			if (oper[a->op].prio == 0) {
+				qnode* newq = (qnode*)malloc(sizeof(qnode));
+				newq->oper = &oper[a->op];
+				newq->back = NULL;
+				addQ(newq, &q);
+			}
+
+			qn->oper->vec.last = qn->oper->vec.last->front;
+			free(a);
+		}
+		free(qn);
+	}
+
+	printf("%d", ans);
 
 	return 0;
 }
