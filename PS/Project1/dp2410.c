8f4263eb07cd81ce87c187e8b1f35e4a38e7267e
Nyapy <nyapy@naver.com>
2022-10-26 01:06:25 +0900

    DP2410 2의멱수의합

---
+++ PS/Project1/dp2410.c
@@ -0,0 +1,43 @@
+#if 01
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int N, k, dp[25][1000001];
+int myuk=1;
+int main() {
+
+	scanf("%d", &N);
+
+	while (myuk <= N) {
+		myuk *= 2;
+	}
+	myuk /= 2;
+
+	k = 2;
+	int cnt = 1;
+	
+	for (int i = 0; i <= N; i++) dp[0][i] = 1;
+
+	while (k <= N) {
+		for (int i = 0; i <= N; i++) {
+			if (i < k) {
+				dp[cnt][i] = dp[cnt - 1][i];
+			}
+			else {
+				dp[cnt][i] = dp[cnt][i - k] + dp[cnt - 1][i];
+			}
+			if (dp[cnt][i] > 1000000000) dp[cnt][i] %= 1000000000;
+		}
+		cnt += 1;
+		k *= 2;
+	}
+
+
+	printf("%d", dp[cnt - 1][N]);
+
+
+	return 0;
+}
+
+#endif
