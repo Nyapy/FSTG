94986302e140ba5573c17624af65c99113ed21b6
Nyapy <nyapy@naver.com>
2022-10-25 23:58:04 +0900

    2225 합분해

---
+++ PS/Project1/DP2225.c
@@ -0,0 +1,29 @@
+#if 01
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int N, K;
+long long dp[201][201];
+
+int main() {
+	scanf("%d %d", &N, &K);
+
+	dp[0][0] = 1;
+	for (int k = 1; k <= K; k++) {
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= i; j++) {
+				dp[k][i] += dp[k - 1][j];
+			}
+			if (dp[k][i] > 1000000000) {
+				dp[k][i] %= 1000000000;
+			}
+		}
+	}
+
+	printf("%d", dp[K][N]);
+
+	return 0;
+}
+
+#endif
