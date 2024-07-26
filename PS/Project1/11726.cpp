f8f4566cd35bc7c989f7f2de9be833ad72b25b6e
Nyapy <52689696+Nyapy@users.noreply.github.com>
2024-07-26 22:23:49 +0900

    11726

---
+++ PS/Project1/11726.cpp
@@ -0,0 +1,23 @@
+#if 1
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int dp[1001];
+
+int tiling(int a) {
+	if (dp[a]) return dp[a];
+	return dp[a] = (tiling(a-2)+ tiling(a-1)) % 10007;
+}
+int n;
+int main() {
+	dp[1] = 1;
+	dp[2] = 2;
+	scanf("%d", &n);
+	printf("%d", tiling(n));
+
+	return 0;
+}
+
+#endif
